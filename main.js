b5d71778e0183cf06b883d902dae88348696408a
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-05-23 01:11:24 -0300

    retirando bugs e adicionando um reset board

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const cards = document.querySelectorAll(".card");
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
     this.classList.add("flip");
 
     if(!hasFlippedCard) {
@@ -28,13 +32,24 @@ function checkForMath() {
 function disableCards() {
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
+
+    resetBoard();
 }
 
 function unflipCard() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove("flip");
         secondCard.classList.remove("flip");
+
+        resetBoard();
     }, 1500)
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => {
